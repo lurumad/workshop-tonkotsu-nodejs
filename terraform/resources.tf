527175c8945fd6df200be60a34f7db14079fea61
lurumad <luis_ruiz_pavon@hotmail.com>
2021-05-25 15:40:51 +0200

    add terraform

---
+++ terraform/resources.tf
@@ -0,0 +1,60 @@
+terraform {
+    backend "azurerm" {
+      resource_group_name = "terraform-rg"
+      storage_account_name = "statesterraform"
+      container_name = "tonkotsu"
+      key = "tonkotsu.tfstate"
+    }
+}
+
+provider "azurerm" {
+  version = "~>2.0"
+}
+
+resource "random_string" "prefix" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = "tonkotsu-rg"
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "${random_string.prefix.result}-plan"
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+
+  kind = "Linux"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  properties {
+    reserved = true
+  }
+}
+
+resource "azurerm_app_service" "dockerapp" {
+  name                = "${random_string.prefix.result}-dockerapp"
+  location            = azurerm_resource_group.group.location
+  resource_group_name = azurerm_resource_group.group.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  app_settings {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  }
+
+  site_config {
+    linux_fx_version = "DOCKER|${var.docker_image}"
+    always_on        = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "location" {
+  default = "west europe"
+}
+
+variable "docker_image" {}
+
